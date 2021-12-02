e628dcdc2e3cd26f4a62562bea7cdbfbe42691c9
freek <freek@spatie.be>
2021-12-03 00:38:35 +0100

    wip

--- app/Commands/WatchCommand.php
+++ app/Commands/WatchCommand.php
@@ -4,8 +4,8 @@ namespace App\Commands;
 
 use App\Support\GitHub\GitHub;
 use App\Support\LocalGitRepo;
+use Exception;
 use LaravelZero\Framework\Commands\Command;
-use League\Flysystem\Adapter\Local;
 use function Termwind\render;
 
 class WatchCommand extends Command
@@ -14,15 +14,59 @@ class WatchCommand extends Command
 
     protected $description = 'Watch the GitHub actions of a repo';
 
-    public function handle(GitHub $gitHub)
+    public function handle(GitHub $gitHub): int
     {
+        $this
+            ->clearScreen()
+            ->renderTitle();
+
         $localGitRepo = new LocalGitRepo(base_path());
 
-        $vendorAndRepo = $localGitRepo->getVendorAndRepo();
+        try {
+            $vendorAndRepo = $localGitRepo->getVendorAndRepo();
+        } catch (Exception $exception) {
+            $this->renderError($exception->getMessage());
+
+            return static::FAILURE;
+        }
 
         $runs = $gitHub->getLatestWorkflowRuns($vendorAndRepo);
 
-        render(view('runs', compact('runs')));
+        foreach(range(1, 5) as $i) {
+            $this
+                ->clearScreen()
+                ->renderTitle()
+                ->renderError("This is pass {$i}");
+
+            render(view('runs', compact('runs')));
+
+            sleep(1);
+        }
+
+        return static::SUCCESS;
+    }
+
+
+
+    public function clearScreen(): self
+    {
+        $this->output->write(sprintf("\033\143"));
+
+        return $this;
+    }
+
+    protected function renderTitle(): self
+    {
+        render("<p class='m-1 bg-green-800 text-white p-1'>GitHub Actions Watcher by Spatie</p>");
+
+        return $this;
+    }
+
+    public function renderError(string $message): self
+    {
+        render("<p class='m-1 bg-red-800 text-white'>{$message}</p>");
+
+        return $this;
     }
 
 }
