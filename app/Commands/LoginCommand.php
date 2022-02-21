6f2c8ba36f7d9b54189f4d99d09ae2319531972a
Freek Van der Herten <freek@spatie.be>
2022-02-21 15:46:03 +0100

    wip

--- app/Commands/LoginCommand.php
+++ app/Commands/LoginCommand.php
@@ -39,7 +39,8 @@ class LoginCommand extends Command
 
         $gitHubUser = $this->gitHub->getAuthorizedUser($accessToken);
 
-        $this->config
+        $this
+            ->config
             ->setAccessToken($accessToken)
             ->setGitHubUsername($gitHubUser['login']);
 
